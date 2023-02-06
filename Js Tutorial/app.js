dc1f0f17e6e8385e8c93f6c41595b215c2e3c595
Axmed Axmedov <55192261+axmed97@users.noreply.github.com>
2023-02-06 17:49:28 +0400

    Add files via upload

---
+++ Js Tutorial/app.js
@@ -0,0 +1,91 @@
+'use strict'
+// alert('Salam, Sayyar?')
+/*
+console.log(2*2)
+
+prompt('senin adin nedi?')
+*/
+
+
+// compile, interpetor
+// meymun = "Maqa"
+
+// console.log("Hello " + userName)
+// userName = 52
+// alert("Hello " + userName)
+
+// age = 29
+// console.log(`Your age ${age}`)
+
+// console.log("Hello " + userName)
+
+// age = 23
+// console.log(`Your age ${age}`)
+
+
+// let, var, const
+
+// var a = 6
+// var b = "salam"
+
+// var c = 1.5, d = 88
+
+// var e = 'hello',
+// f = 'sagol'
+
+// let a = 15
+
+// a = 'Meri - 29'
+
+// console.log(a)
+
+// var b  =' yusif'
+// b = 15
+
+// const Pi = 3.14
+// maqa = 'agzivi bagla'
+// console.log(Pi)
+
+// data types
+
+//  a = "Meri" // String
+//  b = 5, c = 5.6 // Number
+// let d = true, f = false // Boolean
+// let answer = true
+//  answer = (999999999999n*9999999999999999999999999999999n)
+// console.log(a)
+// console.log(typeof b);
+
+
+// let salam
+// console.log(maqa)
+// let symb = Symbol('a')
+// let aym = Symbol('a')
+// console.log(symb == aym)
+// let response = null
+// console.log()
+// primitive  - string, number, boolean, null, symbol, undefiend, bigInt
+
+// let a = 5;
+
+// var salam = alert("Meri");
+// console.log(salam);
+// var name = prompt("Senin adin nedi?");
+// console.log(`Menim adim ${name}`);
+// document.getElementById("text").innerHTML = name
+// let ülviyyə = +prompt("Nece yəıöğş.çüasin var?")
+
+// console.log(2023 - ülviyyə)
+
+// NaN - not a number
+
+let a = 5 , b = 2;
+
+// console.log(a + b); // 7 
+// console.log(a - b); // 3
+// console.log(a * b); // 10
+// console.log(a / b); // 2.5
+// console.log(a % b); // 1
+// console.log(4 % 2); // 0
+
+// console.log("5" % "5");
